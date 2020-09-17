18acec83075a2641a66be8440763730011067506
Abdul Ahad <abdulhanjra@hotmail.com>
2020-09-17 20:19:48 +0500

    App Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFormComponent } from './myForm/my-form/my-form.component';
+import { CreateComponent } from './Products/create/create.component';
+import { DetailComponent } from './Products/detail/detail.component';
+import { EditComponent } from './Products/edit/edit.component';
+import { ListComponent } from './Products/list/list.component';
+import { ProductFormComponent } from './shared/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent,
+    CreateComponent,
+    DetailComponent,
+    EditComponent,
+    ListComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
