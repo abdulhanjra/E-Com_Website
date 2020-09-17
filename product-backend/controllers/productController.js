7d424fd170757abeba6ced36ccc8449797806ba6
Abdul Ahad <abdulhanjra@hotmail.com>
2020-09-17 20:23:38 +0500

    message

---
+++ product-backend/controllers/productController.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import product from '../models/productModel';
+
+exports.get = (req, res) => {
+    product.findById(req.params.id, (err, item) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(item);        
+    });
+};
+
+exports.getAll = (req, res) => {
+    product.find({}, (err, item) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(item);
+    });
+};
+
+exports.post = (req, res) => {
+    const newProduct = new product(req.body);
+
+    newProduct.save((err, item) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(item);
+    });
+};
+
+exports.update = (req, res) => {
+    product.findOneAndUpdate({
+        _id: req.params.id
+    }, req.body,
+        (err, item) => {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(item);
+        });
+};
+
+exports.delete = (req, res) => {
+    product.remove({
+        _id: req.params.id
+    }, (err) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json({
+            message: `product ${req.params.productId} successfully deleted`
+        });
+    });
+};
---
+++ product-backend/routes/index.js
@@ -0,0 +1,12 @@
+import products from '../controllers/productController';
+
+export default(app) => {
+    app.route('/products')
+        .get(products.getAll)
+        .post(products.post);
+
+    app.route('/products/:id')
+        .get(products.get)
+        .put(products.update)
+        .delete(products.delete);
+};
